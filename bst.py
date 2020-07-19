14eb86e3ece3efa3b5c809a5753c1492b77ec8b9
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-07-18 20:18:47 -0700

    bst updated

--- bst.py
+++ bst.py
@@ -3,4 +3,13 @@ class BinarySearchTree(self, value):
         self.value = value
         self.left = None
         self.right = None
-        
+
+    def insert(self, value):
+        if value <= self.value and self.left is None:
+            self.left = BinarySearchTree(value)
+        elif value > self.value and self.right is None:
+            self.right = BinarySearchTree(value)
+        elif value <= self.value and self.left is not None:
+            self.left.insert(value)
+        else:
+            self.right.insert(value)
