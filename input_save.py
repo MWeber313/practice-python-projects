9529fecd127d2d83d8bb28f9cf0a9f22b7a5bd90
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-08-15 22:10:49 -0700

    Set parameters for project

---
+++ input_save.py
@@ -0,0 +1,3 @@
+# This project will take in an input
+# This project will write to a file and save the note based on input
+# EWventually you will be able to read the program (a la 'less' from Linux)
