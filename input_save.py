c6771eb43c2e78ef8d753b862ad1e4521f9edf7a
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-08-15 22:16:49 -0700

    not working - why??

--- input_save.py
+++ input_save.py
@@ -1,3 +1,9 @@
 # This project will take in an input
 # This project will write to a file and save the note based on input
 # EWventually you will be able to read the program (a la 'less' from Linux)
+
+# A simple input
+
+x = input('Please type something here')
+response = F"Hello, you said: {x}"
+print(response)
