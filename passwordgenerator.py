60d78e1f9aec3bc5bf3bf398a36b697c081963c1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-18 23:22:45 -0800

    password prints, but shuffle function not working yet

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,2 +1,40 @@
 import random
 
+# This password generator will take 4 pairs of 4 different types of characters:
+# lowercase, uppercase, integer, special character
+
+# Upper Case 1
+uppercaseLetter1=chr(random.randint(65,90))
+print(uppercaseLetter1)
+
+# Upper Case 2
+uppercaseLetter2=chr(random.randint(65, 90))
+print(uppercaseLetter2)
+
+# Lower Case 1
+lowercaseLetter1=chr(random.randint(97, 122))
+print(lowercaseLetter1)
+
+# Lower Case 2
+lowercaseLetter2=chr(random.randint(97, 122))
+print(lowercaseLetter2)
+
+# Digit 1
+digit1=str(int(random.randint(0, 999)))
+print(digit1)
+
+# Digit 2
+digit2=str(int(random.randint(0, 999)))
+print(digit2)
+
+# Special Character 1
+specialCharacter1=chr(random.randint(33, 96))
+print(specialCharacter1)
+
+# Special Character 2
+specialCharacter2=chr(random.randint(33, 96))
+print(specialCharacter2)
+
+password=uppercaseLetter1+uppercaseLetter2+lowercaseLetter1+lowercaseLetter2+digit1+digit2+specialCharacter1+specialCharacter2
+random.shuffle(password)
+print(password)
