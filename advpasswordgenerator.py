e1845a051737266cbfcbe05f4730e200c35fdd17
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-22 23:51:55 -0800

    Trying to understand a few errors

--- advpasswordgenerator.py
+++ advpasswordgenerator.py
@@ -1,8 +1,10 @@
 # This will be an advanced password generator that will generate a password with a given length
 # You will also decide if you want uppercase letters, lowercase letters, numbers or special characters
 import random
+import math
 # pyperclip will be the copy to clipboard tool
 
+# While loop for the program
 while True:
     print(
         "Welcome to Mack's Password Generator! Please answer a few questions and your password will be generated!"
@@ -24,24 +26,95 @@ while True:
     print("Do you want uppercase letters? Y/N")
 
     upperResponse = input('')
-    if upperResponse == 'Y' or upperResponse == 'y':
-        upperResponseInput == True
-        continue
-    elif upperResponse == 'N' or upperResponse == 'n':
-        upperResponseInput == False
-        continue
-    else:
-        print("Please use only Y or N, case does not matter.")
+
+    while True:
+        if upperResponse != str:
+            print("Response must be a string")
+            break
+        if upperResponse == 'Y' or upperResponse == 'y':
+            upperResponseInput = True
+            continue
+        elif upperResponse == 'N' or upperResponse == 'n':
+            upperResponseInput = False
+            continue
+        else:
+            print("Please use only Y or N, case does not matter.")
 
     print("Do you want lowercase letters? Y/N")
 
     lowerResponse = input('')
 
+    while True:
+        if lowerResponse != str:
+            print("Response must be a string")
+            break
+        if lowerResponse == 'Y' or lowerResponse == 'y':
+            lowerResponseInput = True
+            continue
+        elif lowerResponse == 'N' or lowerResponse == 'n':
+            lowerResponseInput = False
+            continue
+        else:
+            print("Please use only Y or N, case does not matter.")
+
     print("Do you want numbers/integers?")
 
     integerResponse = input('')
+
+    while True:
+        if integerResponse != str:
+                print("Response must be a string")
+                break
+        if integerResponse == 'Y' or integerResponse == 'y':
+            integerResponseInput = True
+            continue
+        elif integerResponse == 'N' or integerResponse == 'n':
+            integerResponseInput = False
+            continue
+        else:
+            print("Please use only Y or N, case does not matter.")
 
     print("Do you want special characters?")
 
     specCharResponse = input('')
 
+    while True:
+        if specCharResponse != str:
+            print("Response must be a string")
+            break
+        if specCharResponse == 'Y' or specCharResponse == 'y':
+            specCharResponseInput = True
+            continue
+        elif specCharResponse == 'N' or specCharResponse == 'n':
+            specCharResponseInput = False
+            continue
+        else:
+            print("Please use only Y or N, case does not matter.")
+
+    break
+
+while True:
+    count = 0
+    if upperResponseInput == True:
+        count +=1
+    else:
+        continue
+    
+    if lowerResponseInput == True:
+        count +=1
+    else:
+        continue
+
+    if integerResponseInput == True:
+        count +=1
+    else:
+        continue
+
+    if specCharResponseInput == True:
+        count +=1
+    else:
+        continue
+
+    totalAvg = (totalAvg // count)
+
+    print(totalAvg)
