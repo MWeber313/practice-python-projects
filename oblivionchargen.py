ea3fd51513d157b5f58f4ba2edf0c96fcda50cac
Mackenzie Weber <mack.webb37@gmail.com>
2021-10-22 20:14:15 -0700

    Oblivion char generator started, race option chosen

---
+++ oblivionchargen.py
@@ -0,0 +1,16 @@
+# This will generate a brand new character for gameplay in Oblivion
+import random
+
+raceChoice = random.choice([
+    'Argonian', 
+    'Breton',
+    'Dark Elf',
+    'High Elf',
+    'Imperial',
+    'Khajit',
+    'Nord',
+    'Orc',
+    'Wood Elf'
+    ])
+
+print(raceChoice)
