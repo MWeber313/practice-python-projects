891289becd4d4d98006573d997e3ffc38a792fb8
Mackenzie Weber <mack.webb37@gmail.com>
2021-10-22 20:18:45 -0700

    Oblivion char generator continued

--- oblivionchargen.py
+++ oblivionchargen.py
@@ -14,3 +14,29 @@ raceChoice = random.choice([
     ])
 
 print(raceChoice)
+
+classChoice = random.choice([
+    'Acrobat',
+    'Agent',
+    'Archer',
+    'Assassin',
+    'Barbarian',
+    'Bard',
+    'Battlemage',
+    'Crusader',
+    'Healer',
+    'Knight',
+    'Mage',
+    'Monk',
+    'Nightblade',
+    'Pilgrim',
+    'Rogue',
+    'Scout',
+    'Sorcerer',
+    'Spellsword',
+    'Thief',
+    'Warrior',
+    'Witchhunter'
+])
+
+print(classChoice)
